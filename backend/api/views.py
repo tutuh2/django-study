226dc46ca0e14daf09e6b00cee100affca6a542a
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-11-16 10:26:40 +0900

    8

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,20 @@
+import json
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET)
+    body = request.body
+    data = {}
+    try: 
+        data = json.loads(body)
+    except:
+        pass
+    print(data.keys())
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
 
