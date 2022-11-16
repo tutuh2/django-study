b46d2e8c8dd0a9cabe592cd394c5dd3d34deff64
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-11-16 10:14:02 +0900

    7

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,10 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})
 print(get_response.text)
+print(get_response.status_code)
 print(get_response.json())
