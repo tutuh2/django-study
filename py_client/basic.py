0cb450548d87c22e566867435645c418cf577209
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-11-16 10:02:22 +0900

    1

---
+++ py_client/basic.py
@@ -0,0 +1,8 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+endpoint = "https://httpbin.org/anything"
+
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+print(get_response.text)
+print(get_response.json())
