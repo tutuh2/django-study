343f0a1cac10555d6653684f327c3ebdce6e4946
Junyoung Song <junyoungsong@Junyoungui-MacBookAir.local>
2022-11-16 15:34:53 +0900

    22

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,17 @@
-import json
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import JsonResponse
 from products.models import Product
+from products.serializers import ProductSerializer
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
+    
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
+get_response = requests.post(endpoint, json={"title": "abc", "content": "Hello world"})
 # print(get_response.text)
 # print(get_response.status_code)
 print(get_response.json())
